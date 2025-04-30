6c2573c76af534df005cd73ea8c921a60dd9d00b
Mark Bush <busht24@gmail.com>
2025-04-30 09:08:19 -0400

    Initial Commit

---
+++ Flavor_Hood/Flavor_HoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flavor_HoodApp.swift
+//  Flavor_Hood
+//
+//  Created by MARK BUSH on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Flavor_HoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
